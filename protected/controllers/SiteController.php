bb072f7e6e79b5b0cd5da2af73f54b96dc914d27
ph7pal <ph7pal@qq.com>
2015-07-08 10:23:19 +0800

    backup and upload 0708

--- protected/controllers/SiteController.php
+++ protected/controllers/SiteController.php
@@ -93,25 +93,53 @@ class SiteController extends Q {
     }
 
     public function actionSitemap() {
-        $page = $_GET['id'];
-        $page = isset($page) ? $page : 1;
-        $dir = Yii::app()->basePath . '/runtime/site/sitemap' . $page . '.xml';
+        if (!zmf::uid()) {
+            $this->redirect(zmf::config('domain'));
+        }
         $a = $_GET['a'];
-        $obj = new Sitemap();
-        if ($a == 'update' || !file_exists($dir)) {
-            $limit = 10000;
-            $start = ($page - 1) * $limit;
-            $rss = $obj->show(Posts::CLASSIFY_POST, $start, $limit);
-            if ($rss) {
-                $obj->saveToFile($dir);
+        $basedir = Yii::app()->basePath . '/../attachments/sitemap/';
+        zmf::createUploadDir($basedir);
+        $total = Chengyu::model()->count('status=' . Posts::STATUS_PASSED);
+        $perFileNum = 1000; //每个文件的连接数
+        $now = zmf::time('', 'Y-m-d');
+        $page = isset($_GET['page']) ? $_GET['page'] : 1;
+        if ($a == 'indexer') {
+            $dir = $basedir . 'indexer.xml';
+            $str = '';
+            for ($i = 1; $i <= ($total / $perFileNum + 1); $i++) {
+                $str.="<sitemap><loc>http://ciyuxinjie.org/attachments/sitemap/sitemap{$i}.xml</loc><lastmod>{$now}</lastmod></sitemap>";
+            }
+            $content = <<<SITEMAP
+<?xml version="1.0" encoding="UTF-8"?>\r\n
+<sitemapindex>$str</sitemapindex>\r\n
+SITEMAP;
+            file_put_contents($dir, $content);
+            exit();
+        } elseif ($a == 'create') {
+            $criteria = new CDbCriteria();
+            $criteria->condition = 'status=' . Posts::STATUS_PASSED;
+            $criteria->select = 'id';
+            $criteria->order = 'id ASC';
+            $criteria->limit = $perFileNum;
+            $criteria->offset = ($page - 1) * $perFileNum;
+            $items = Chengyu::model()->findAll($criteria);
+            if (!empty($items)) {
+                foreach ($items as $v) {
+                    $str.="<url><loc>http://ciyuxinjie.org/detail/{$v['id']}</loc><lastmod>{$now}</lastmod><changefreq>weekly</changefreq><priority>0.8</priority></url>";
+                }
+                $dir = $basedir . "sitemap{$page}.xml";
+                $content = <<<SITEMAP
+<?xml version='1.0' encoding='UTF-8'?>\r\n
+<urlset>{$str}</urlset>
+SITEMAP;
+                file_put_contents($dir, $content);
+                $this->message(1, "正在处理{$page}页", Yii::app()->createUrl('site/sitemap', array('a' => 'create', 'page' => ($page + 1))));
             } else {
-                exit($page . '-->empty');
+                exit('well done');
             }
         } else {
-            $rss = $obj->getFile($dir);
+            exit('不允许的操作');
         }
-        //rss创建
-        $this->render('//site/sitemap', array('rss' => $rss));
     }
 
 }
